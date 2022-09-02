e96d62f6e278eda935893fae5a5541c690e3ebb5
rudyak <rudyakmaxl@gmail.com>
2022-09-02 12:47:49 +0300

    add mro python file

--- lessons/lesson_1_mro/classwork/mro.py
+++ lessons/lesson_1_mro/classwork/mro.py
@@ -1,5 +1,6 @@
-'''Multiple inheritance and "MRO" example. 
-   More information about "MRO" -> https://tirinox.ru/mro-python/ '''
+"""Multiple inheritance and "MRO" example. 
+   More information about "MRO" -> https://tirinox.ru/mro-python/ """
+
 
 class A(object):
     def name(self):
@@ -16,33 +17,35 @@ class C(A):
         print("I'm C")
 
 
-'''You can inherit from class b and c but you can't inherit from class a'''
+"""You can inherit from class b and c but you can't inherit from class a"""
+
+
 class D(B, C):
     def name(self):
         print("I'm D")
-        
-        
+
+
 # This is a mistake
 
-'''
+"""
 class D(A, B):
     def name(self):
         print("I'm D")
-'''
+"""
 
-'''
+"""
 class D(A, C):
     def name(self):
         print("I'm D")
-'''
+"""
 
 
 def print_mro(T):
-    '''It's function "print_mro" for to write only class names'''
-    print(*[c.__name__ for c in T.mro()], sep=' -> ')
+    """It's function "print_mro" for to write only class names"""
+    print(*[c.__name__ for c in T.mro()], sep=" -> ")
 
 
 print_mro(D)
-#print(D.__mro__)
+# print(D.__mro__)
 # d = D()
 # d.name()
